56433021cbd6f4cef1e018278a5a0aadcd942f31
Atsushi <atsushimiyamoto07@gmail.com>
2021-03-07 14:55:21 -0800

    create project

--- server/animalCollector/asgi.py
+++ server/animalCollector/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for animalCollection project.
+ASGI config for animalCollector project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'animalCollection.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'animalCollector.settings')
 
 application = get_asgi_application()
--- server/animalCollector/wsgi.py
+++ server/animalCollector/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for animalCollection project.
+WSGI config for animalCollector project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'animalCollection.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'animalCollector.settings')
 
 application = get_wsgi_application()
