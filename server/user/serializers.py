1d92b62a318b046b83059aedf604028aa3a5142e
Atsushi <atsushimiyamoto07@gmail.com>
2021-03-07 19:52:32 -0800

    Create signup serializer

---
+++ server/user/serializers.py
@@ -0,0 +1,28 @@
+from django.contrib.auth import get_user_model, authenticate
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.ModelSerializer):
+    password1 = serializers.CharField(write_only=True)
+    password2 = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        if data['password1'] != data['password2']:
+            raise serializers.ValidationError('Password must be match')
+        return data
+
+    def create(self, validated_data):
+        data = {
+            key: value for key, value in validated_data.items()
+            if key not in ('password1', 'password2')
+        }
+        data['password'] = validated_data['password1']
+
+        return self.Meta.model.objects.create_user(**data)
+
+    class Meta:
+        model = get_user_model()
+        fields = (
+            'id', 'email', 'password1', 'password2'
+        )
+        read_only_fields = ('id',)
