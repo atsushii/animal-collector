43b2fa919dbaab814fad03a6cfe2863e351ff1e5
Atsushi <atsushimiyamoto07@gmail.com>
2021-03-09 14:20:15 -0800

    Created admin page for useranimal and animal models

--- server/core/admin.py
+++ server/core/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-from core.models import User
+from core.models import User, Animal, UserAnimal
 
 
 @admin.register(User)
@@ -19,4 +19,31 @@ class UserAdmin(BaseUserAdmin):
             'classes': ('wide',),
             'fields': ('email', 'password1', 'password2')
         }),
+    )
+
+
+@admin.register(Animal)
+class AnimalAdmin(admin.ModelAdmin):
+    fields = (
+        'id', 'animal_name', 'description',
+    )
+    list_display = (
+        'id', 'animal_name', 'description',
+    )
+    readonly_fields = (
+        'id',
+    )
+
+@admin.register(UserAnimal)
+class UserAnimalAdmin(admin.ModelAdmin):
+    fields = (
+        'id', 'users', 'animals', 'picture_url',
+        'x_coordinate', 'y_coordinate', 'created_date'
+    )
+    list_display = (
+        'id', 'picture_url',
+        'x_coordinate', 'y_coordinate', 'created_date'
+    )
+    readonly_fields = (
+        'id', 'created_date'
     )
