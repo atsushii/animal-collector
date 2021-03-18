ff8b1740c70d8ea820253f32bc821240e3c284d6
Atsushi <atsushimiyamoto07@gmail.com>
2021-03-17 20:55:24 -0700

    extract animal name and description from wikipedia

--- server/animal/wikipedia/api.py
+++ server/animal/wikipedia/api.py
@@ -8,14 +8,32 @@ class AnimalDataProcessing:
         self.animal_name = animal_name
 
     def parse_html(self):
-        page = requests.get(self.url)
-        soup = BeautifulSoup(page.content, 'html.parser')
-        name = soup.find(id='firstHeading')
-        print(name.text)
+        try:
+            page = requests.get(self.url)
+            soup = BeautifulSoup(page.content, 'html.parser')
+        except requests.exceptions.RequestException as err:
+            raise SystemExit(err)
 
+        return soup
+
+    def extract_animal_name_and_description(self, class_id='firstHeading', soup=None, class_name='mw-parser-output'):
+        try:
+            animal_name = soup.find(id=class_id).text
+            p_tag_list = soup.find('div', class_=class_name)
+            description = p_tag_list.findChildren('p', recursive=False)[2].text
+        except AttributeError as err:
+            print(err)
+
+        return animal_name, description
+
+    def main(self):
+        soup = self.parse_html()
+        animal_name, description = self.extract_animal_name_and_description(soup=soup)
+
+        return animal_name, description
 
 if __name__=='__main__':
-    a = AnimalDataProcessing('cat')
-    a.parse_html()
+    a = AnimalDataProcessing('tiger')
+    a.main()
 
 
