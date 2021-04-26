e168b4d78dcf290283693500031d61cb7f029c5a
Atsushi <atsushimiyamoto07@gmail.com>
2021-04-26 15:05:10 -0700

    Run test for register user

--- server/user/tests/test_user.py
+++ server/user/tests/test_user.py
@@ -1,5 +1,34 @@
-import pytest_django
+from django.test import TestCase
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 
-CREATE_USER_URL = reverse('')
+from rest_framework.test import APIClient
+from rest_framework import status
+
+CREATE_USER_URL = reverse('user:sign-up')
+LOGIN_URL = reverse('user:log-in')
+DELETE_USER_URL = reverse('user:delete')
+
+
+def create_user(**kwargs):
+    return  get_user_model().objects.create_user(**kwargs)
+
+
+class UserApiTests(TestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_create_user(self):
+        payload = {
+            'email': 'test@gmail.com',
+            'password1': 'passWord@1',
+            'password2': 'passWord@1'
+        }
+
+        response = self.client.post(CREATE_USER_URL, payload)
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        user = get_user_model().objects.get(**response.data)
+        self.assertTrue(user.check_password(payload['password1']))
+        self.assertNotIn('password', response.data)
