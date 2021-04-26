ccf7dc7dcd6b505d865f4a3e24478829064a71a2
Atsushi <atsushimiyamoto07@gmail.com>
2021-04-26 16:08:40 -0700

    Run test for rregister user

--- server/user/tests/test_user.py
+++ server/user/tests/test_user.py
@@ -33,7 +33,7 @@ class UserApiTests(TestCase):
         self.assertTrue(user.check_password(payload['password1']))
         self.assertNotIn('password', response.data)
 
-    def test_create_user_unsuccess(self):
+    def test_create_user_invalid_password(self):
         payload = {
             'email': 'test@gmail.com',
             'password1': 'password1',
@@ -41,5 +41,23 @@ class UserApiTests(TestCase):
         }
 
         response = self.client.post(CREATE_USER_URL, payload)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_user_exist(self):
+        payload1 = {
+            'email': 'test@gmail.com',
+            'password': 'passWord@1',
+        }
+
+        payload2 = {
+            'email': 'test@gmail.com',
+            'password1': 'passWord@1',
+            'password2': 'passWord@1'
+        }
+
+        create_user(**payload1)
+
+        response = self.client.post(CREATE_USER_URL, payload2)
 
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
