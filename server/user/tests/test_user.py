66ef53c22f73cf221e5a614e4e80e7ba4b03ea9b
Atsushi <atsushimiyamoto07@gmail.com>
2021-04-26 16:04:08 -0700

    Run test for register user

--- server/user/tests/test_user.py
+++ server/user/tests/test_user.py
@@ -19,7 +19,7 @@ class UserApiTests(TestCase):
     def setUp(self):
         self.client = APIClient()
 
-    def test_create_user(self):
+    def test_create_user_success(self):
         payload = {
             'email': 'test@gmail.com',
             'password1': 'passWord@1',
@@ -32,3 +32,14 @@ class UserApiTests(TestCase):
         user = get_user_model().objects.get(**response.data)
         self.assertTrue(user.check_password(payload['password1']))
         self.assertNotIn('password', response.data)
+
+    def test_create_user_unsuccess(self):
+        payload = {
+            'email': 'test@gmail.com',
+            'password1': 'password1',
+            'password2': 'password2'
+        }
+
+        response = self.client.post(CREATE_USER_URL, payload)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
