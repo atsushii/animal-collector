e289567cc70e84c5d8ffb8cd578dd87b7a2b2e1c
Atsushi <atsushimiyamoto07@gmail.com>
2021-05-09 09:31:31 -0700

    fixed authentication forms

--- client/src/components/form/Signup.js
+++ client/src/components/form/Signup.js
@@ -1,26 +1,71 @@
 import React from "react";
-import useSignUpForm from "./useSignUpForm";
+import { useState } from "react";
 
-const Signup = () => {
-    const {inputs, handleInputChange, handleSubmit} = useSignUpForm();
+
+const Signup = ({ onAuthenticate }) => {
+    const [email, setEmail] = useState("");
+    const [password1, setPasssword1] = useState("");
+    const [password2, setPassword2] = useState("");
+
+    async function registerUser() {
+        const url = 'http://localhost:8003/api/user/sign-up/'
+        await axios.post(
+            url,
+            email,
+            password1,
+            password2
+        )
+        .then((response) => {
+            // Todo
+            // Return access token from server when user signup
+            const { use, accessTokenr } = response.data();
+            onAuthenticate(user, accessToken)
+
+        }, (error) => {
+            console.log(error)
+        });
+    }
+
+
+    const handleSubmit = async (e) =>  {
+        e.preventDefault();
+        registerUser();
+    }
 
     return (
         <form onSubmit={handleSubmit}>
-            <div>
-                <label>email</label>
-                <input type="email" name="email" onChange={handleInputChange}
-                value={inputs.email} required />
-            </div>
-            <div>
-                <label>Password</label>
-                <input type="password" name="password1" onChange={handleInputChange}
-                value={inputs.password1} required />
-            </div>
-            <div>
-                <label>re-enter Password</label>
-                <input type="password" name="password2" onChange={handleInputChange}
-                value={inputs.password2} required />
-            </div>
+            <fieldset>
+                <label>
+                    email
+                    <input 
+                        type="email" 
+                        value={ email } 
+                        onChange={(e) => setEmail(e.currentTarget.value)}
+                        required 
+                    />
+                </label>
+            </fieldset>
+            <fieldset>
+                <label>
+                    Password
+                <input 
+                    type="password" 
+                    value={ password1 } 
+                    onChange={(e) => setPasssword1(e.currentTarget.value)}
+                    required 
+                />
+                </label>
+            </fieldset>
+            <fieldset>
+                <label>
+                    re-enter Password
+                    <input 
+                        type="password" 
+                        value={ password2 } 
+                        onChange={(e) => setPasssword2(e.currentTarget.value)}
+                        required />
+                </label>
+            </fieldset>
             <button type="submit">Sign Up</button>
         </form>
     );
