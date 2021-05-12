0e6977497ae9e713173991d2358e11924eb81a1d
Atsushi <atsushimiyamoto07@gmail.com>
2021-05-12 13:11:11 -0700

    fixed props

--- client/src/components/form/AuthenticationForm.js
+++ client/src/components/form/AuthenticationForm.js
@@ -18,7 +18,7 @@ export const AuthenticationForm = () => {
         }
     }, []);
 
-    const handleAuth = ({ user, token }) => {
+    const handleAuth = ( user, token ) => {
         logIn(user, token)
         // use .replace incstead of .push to avoid return login page
         history.replace(location.state?.form || "/")
